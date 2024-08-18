0ea4105ded7391413e4577099ae6cc83cfabab98
Kyriet <kyriet.sebastian@gmail.com>
2024-08-18 12:38:42 +0200

    Don't show console for Release build

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,22 @@
 #include "pch.hpp"
 #include "App.hpp"
 
-int main()
+
+int main(int, char**)
 {
-	auto app = std::make_unique<App>(AppConfig{});
-	app->Run();
+	App app(AppConfig{});
+	app.Run();
 }
+
+// On Windows for Release build we don't want the console window to appear.
+
+#ifdef _WIN32
+#ifdef NDEBUG
+#pragma comment(linker, "/SUBSYSTEM:WINDOWS")
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+	return main(__argc, __argv);
+}
+#endif
+#endif
